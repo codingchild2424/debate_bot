b4eff0db2611fa208dc9dff3d3390d0fb6883d14
Junbo Koh <cjsn92@gmail.com>
2023-05-15 23:22:48 +0900

    refactor: add role params in function

--- modules/gpt_modules.py
+++ modules/gpt_modules.py
@@ -1,5 +1,4 @@
 import openai
-import os
 
 from dotenv import dotenv_values
 
@@ -8,14 +7,11 @@ config = dotenv_values(".env")
 openai.organization = config.get('OPENAI_ORGANIZATION')
 openai.api_key = config.get('OPENAI_API_KEY')
 
-# openai.organization = os.environ['OPENAI_ORGANIZATION']
-# openai.api_key = os.environ['OPENAI_API_KEY']
-
-def gpt_call(prompt):
+def gpt_call(prompt, role="user"):
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=[
-                    {"role": "user", "content": prompt},
+                    {"role": role, "content": prompt},
                 ]
     )
     output_text = response["choices"][0]["message"]["content"]
